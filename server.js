11c9b8721e79bfb31595155e7699566ada78f875
Jorge <jorgegvilla98@gmail.com>
2021-05-05 21:07:32 +0200

    api crud sneakers created

--- server.js
+++ server.js
@@ -1,15 +1,28 @@
 const express = require('express');
 const mysql = require("mysql");
 const myconn = require("express-myconnection");
-
 const app = express();
+const routes = require('./routes');
+const dbOptions = {
+    host : 'localhost',
+    port : 3306,
+    user : 'root',
+    password : '',
+    database : 'sneakerpop'
+}
 // Config
 app.set('port', process.env.PORT || 9000);
+
+//Middlewares
+app.use(myconn(mysql, dbOptions, "single"));
+app.use(express.json());
+// Router Api
+app.get('/',(req,send)=>{
+    send.send('hola')
+})
+app.use('/api', routes);
+
 
 app.listen(app.get('port'),()=>{
     console.log("El server esta iniciado", app.get('port'));
 });
-// Routes 
-app.get('/',(req,res)=>{
-    res.send('Inicio de la api')
-})
