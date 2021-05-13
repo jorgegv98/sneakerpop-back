85fdb2d6fdecfe7af0b6049d46f959cbd8b2d4a7
Jorge <jorgegvilla98@gmail.com>
2021-05-13 22:14:38 +0200

    add jwt token and bcrypt pass

--- routes.js
+++ routes.js
@@ -1,35 +1,105 @@
-const express = require('express');
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const verify = require("jsonwebtoken/verify");
+const bcrypt = require("bcryptjs");
 const routes = express.Router();
 
-// Routes 
+// Routes
+routes.post("/login",(req, res) => {
+  req.getConnection((err, conn) => {
+    if (err) return res.send(err);
+    conn.query(
+      "SELECT * FROM users WHERE email = ? ",
+      [req.body.email],
+      async (err, rows) => {
+        if (err) return res.send(err);
+        if (rows) {
+          const validPass = await bcrypt.compare(req.body.password, rows[0].password);
+          if (validPass) {
+            jwt.sign({ user: rows }, "secretKey", (error, token) => {
+              res.json({
+                user: rows[0].email,
+                token: token,
+              });
+            });
+          } else {
+            res.json({
+              error: "Password incorrecta",
+            });
+          }
+        } else {
+          res.json({
+            error: "Email o password incorrectos",
+          });
+        }
+      }
+    );
+  });
+});
+
+routes.post("/register", async (req, res) => {
+  let user = req.body.email;
+  let rol = req.body.rol;
+  let password = await bcrypt.hash(req.body.password, 10);
+  req.getConnection((err, conn) => {
+    if (err) return res.send(err);
+    conn.query(
+      "INSERT INTO users (email, password, rol) VALUES ( ?, ?, ? )",
+      [user, password, rol],
+      (err, rows) => {
+        if (err) return res.send(err);
+        res.json("Usuario creado correctamente");
+      }
+    );
+  });
+});
 
 // -------------------------------- Sneakers ---------------------------------------------------
-routes.get('/sneakers',(req,res)=>{
-    req.getConnection((err,conn)=>{
+routes.get("/sneakers", (req, res) => {
+  token = req.headers["authorization"];
+  if (!token) {
+    res.status(401).send({
+      error: "Necesario autentificacion",
+    });
+    return;
+  }
+  jwt.verify(token, "secretKey", (error, user) => {
+    if (error) {
+      res.json({
+        error: "Token Invalido",
+      });
+    } else {
+      req.getConnection((err, conn) => {
         if (err) return res.send(err);
 
-        conn.query('SELECT * FROM sneakers',(err,rows)=>{
-        if (err) return res.send(err);
-            res.json(rows);
-        })
-    })
+        conn.query("SELECT * FROM sneakers", (err, rows) => {
+          if (err) return res.send(err);
+          res.json(rows);
+        });
+      });
+    }
+  });
 });
 
 routes.get("/sneaker/:id", (req, res) => {
   req.getConnection((err, conn) => {
     if (err) return res.send(err);
 
-    conn.query("SELECT * FROM sneakers WHERE id = ?", [req.params.id] ,(err, rows) => {
-      if (err) return res.send(err);
-      res.json(rows);
-    });
+    conn.query(
+      "SELECT * FROM sneakers WHERE id = ?",
+      [req.params.id],
+      (err, rows) => {
+        if (err) return res.send(err);
+        res.json(rows);
+      }
+    );
   });
 });
 
 routes.post("/sneaker", (req, res) => {
   req.getConnection((err, conn) => {
     if (err) return res.send(err);
-    conn.query('INSERT INTO sneakers set ?',[req.body],(err, rows) => {
+    conn.query("INSERT INTO sneakers set ?", [req.body], (err, rows) => {
       if (err) return res.send(err);
       res.json(req.body);
     });
@@ -39,10 +109,14 @@ routes.post("/sneaker", (req, res) => {
 routes.delete("/sneaker/:id", (req, res) => {
   req.getConnection((err, conn) => {
     if (err) return res.send(err);
-    conn.query("DELETE FROM sneakers WHERE id = ?", [req.params.id], (err, rows) => {
-      if (err) return res.send(err);
-      res.send("Zapatilla eliminada correctamente");
-    });
+    conn.query(
+      "DELETE FROM sneakers WHERE id = ?",
+      [req.params.id],
+      (err, rows) => {
+        if (err) return res.send(err);
+        res.send("Zapatilla eliminada correctamente");
+      }
+    );
   });
 });
 
@@ -51,7 +125,7 @@ routes.post("/sneaker/:id", (req, res) => {
     if (err) return res.send(err);
     conn.query(
       "UPDATE sneakers set ? WHERE id = ?",
-      [req.body,req.params.id],
+      [req.body, req.params.id],
       (err, rows) => {
         if (err) return res.send(err);
         res.send("Zapatilla actualizada correctamente");
@@ -87,13 +161,16 @@ routes.get("/brand/:id", (req, res) => {
   req.getConnection((err, conn) => {
     if (err) return res.send(err);
 
-    conn.query("SELECT * FROM brands WHERE id = ?",[req.params.id],(err, rows) => {
-      if (err) return res.send(err);
-      res.json(rows);
-    });
+    conn.query(
+      "SELECT * FROM brands WHERE id = ?",
+      [req.params.id],
+      (err, rows) => {
+        if (err) return res.send(err);
+        res.json(rows);
+      }
+    );
   });
 });
-
 
 //--------------------------------------- Raffles -------------------------------------------------------------------------
 
@@ -122,12 +199,26 @@ routes.get("/raffle/:id", (req, res) => {
   req.getConnection((err, conn) => {
     if (err) return res.send(err);
 
-    conn.query("SELECT * FROM brands WHERE id = ?",[req.params.id],(err, rows) => {
-      if (err) return res.send(err);
-      res.json(rows);
-    });
+    conn.query(
+      "SELECT * FROM brands WHERE id = ?",
+      [req.params.id],
+      (err, rows) => {
+        if (err) return res.send(err);
+        res.json(rows);
+      }
+    );
   });
 });
 
+function verifyToken(req, res, next) {
+  const tokenHeader = req.headers["authorization"];
+  if (typeof tokenHeader !== "undefined") {
+    const token = tokenHeader.split(" ")[1];
+    req.token = token;
+    next();
+  } else {
+    res.status(403);
+  }
+}
 
 module.exports = routes;
